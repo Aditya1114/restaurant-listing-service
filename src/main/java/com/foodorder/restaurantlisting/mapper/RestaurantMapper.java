9343e98340b9aa668751db80fc043e62e21ab7c1
Aditya Saha <aadi@Adityas-MacBook-Air.local>
2024-10-09 23:42:42 +0530

    first commit

---
+++ src/main/java/com/foodorder/restaurantlisting/mapper/RestaurantMapper.java
@@ -0,0 +1,17 @@
+package com.foodorder.restaurantlisting.mapper;
+
+import com.foodorder.restaurantlisting.dto.RestaurantDTO;
+import com.foodorder.restaurantlisting.entity.Restaurant;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface RestaurantMapper {
+
+    RestaurantMapper INSTANCE = Mappers.getMapper(RestaurantMapper.class);
+
+    Restaurant mapRestaurantDTOToRestaurant(RestaurantDTO restaurantDTO);
+
+    RestaurantDTO mapRestaurantToRestaurantDTO(Restaurant restaurant);
+
+}
---
+++ src/main/java/com/foodorder/restaurantlisting/repo/RestaurantRepo.java
@@ -0,0 +1,9 @@
+package com.foodorder.restaurantlisting.repo;
+
+import com.foodorder.restaurantlisting.entity.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepo extends JpaRepository<Restaurant, Integer> {
+}
